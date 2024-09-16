66735359659c59c5c658613b83822ae46d9fb7e6
EmanueleHamzaraj-Ciellos <emanuele.hamzaraj@ciellos.com>
2024-09-16 14:25:00 +0200

    fix ps func

--- compose-mail/ComposeEmail.ps1
+++ compose-mail/ComposeEmail.ps1
@@ -1,11 +1,17 @@
 # .github/actions/compose-email/ComposeEmail.ps1
+function ComposeEmail {
 
-param(
-    [string]$Message  # Input parameter for the message
-)
-
-# Compose the email body
-$emailBody = "Composed email body: $Message"
-
-# Set the output for the composed email body
-Write-Output "email_body=$emailBody" >> $env:GITHUB_OUTPUT
+    param(
+        [string]$Message  # Input parameter for the message
+    )
+     $outputFile = "$env:GITHUB_OUTPUT"
+    # Compose the email body
+    $emailBody = "Composed email body: $Message"
+    
+    # Set the output for the composed email body
+    #Write-Output "email_body=$emailBody" >> $env:GITHUB_OUTPUT
+    Add-Content -Path $outputFile -Value "email_body=$emailBody"
+    Write-Output "$emailBody"
+    }
+    
+    ComposeEmail -Message $Message
