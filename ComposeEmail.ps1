03853520b65b75995af48a95b02a2d49fd927bf1
EmanueleHamzaraj-Ciellos <emanuele.hamzaraj@ciellos.com>
2024-09-20 15:44:55 +0200

    go from dumbitdown to full

--- ComposeEmail.ps1
+++ ComposeEmail.ps1
@@ -1,6 +1,7 @@
 # .github/actions/compose-mail/ComposeEmail.ps1
 
 param(
+    [Parameter(HelpMessage = "The GitHub Token running the action", Mandatory = $true)]
     [string]$Message,
     [string]$RunName,
     [string]$RunId
@@ -12,11 +13,26 @@ Write-Output "Received workflow run name: $RunName"
 Write-Output "Received workflow run ID: $RunId"
 
 # Process the message
-$emailBody = "Composed email body: $Message, Run Name: $RunName, Run ID: $RunId"
+$emailBody = @"
+    <html>
+    <body>
+    <h1>Workflow Errors</h1>
+    <p>The following errors were found during the run of: $RunName with id: $RunId</p>
+    <pre>$Message</pre>
+    </body>
+    </html>
+"@
+
+# Encode the email body in base64 to avoid issues with special characters
+$emailBodyBase64 = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($emailBody))
+
+# Set the output for the email body in base64
+Write-Output "Generated Email Body:"
+Write-Output $emailBodyBase64
 
 # Set the output for the composed email body
-$outputFile = "$env:GITHUB_OUTPUT"
-Add-Content -Path $outputFile -Value "email_body=$emailBody`n"
+Add-Content -Path $env:GITHUB_OUTPUT -Value "email_body=$emailBody`n"
+Add-Content -Path $env:GITHUB_ENV -Value "email_body=$emailBody`n"
 
 # Debug: Print the composed email body
 Write-Output "Composed Email Body: $emailBody"
